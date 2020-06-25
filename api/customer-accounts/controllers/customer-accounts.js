61bb15c32948454ffce7150a77a60872498f32da
Hà Giang <ck.hagiang@gmail.com>
2020-06-25 22:48:41 +0700

    Create API login
    Create Fucntion Hashpassword And Auth

--- api/customer-accounts/controllers/customer-accounts.js
+++ api/customer-accounts/controllers/customer-accounts.js
@@ -5,4 +5,24 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async auth(ctx){
+        var userName = ctx.request.body.username;
+        var password = ctx.request.body.password;
+        var user  = await strapi.services["customer-accounts"].findOne({user_name: userName});
+        var compare = await strapi.services["customer-accounts"].verifyPassword(password,user.password);
+        if(!user || !compare)
+        {
+            ctx.status(401);
+        }
+        else
+        {
+            ctx.send({
+                status: 200,
+                result : user
+
+           });
+        }
+    }
+
+};
--- api/customer-accounts/models/customer-accounts.js
+++ api/customer-accounts/models/customer-accounts.js
@@ -5,4 +5,11 @@
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+    lifecycles: {
+        async beforeCreate(data) {
+          const passwordHashed = await strapi.services["customer-accounts"].hashPassword(data.password);
+          data.password = passwordHashed;
+        },
+      },
+};
