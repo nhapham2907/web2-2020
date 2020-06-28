9db374e046d44e1a163d219c8110e5e05f38538f
Tuan <anhtuanhoang2312@gmail.com>
2020-06-28 19:36:54 +0700

    Add Interest rates, Spend accounts

---
+++ api/interest-rate/controllers/interest-rate.js
@@ -0,0 +1,17 @@
+'use strict';
+// const { sanitizeEntity } = require("strapi-utils");
+
+module.exports = {
+  // async find(ctx) {
+  //   let entities;
+  //   if (ctx.query._q) {
+  //     entities = await strapi.services['interest-rate'].search(ctx.query);
+  //   } else {
+  //     entities = await strapi.services['interest-rate'].find(ctx.query);
+  //   }
+
+  //   return entities.map((entity) =>
+  //     sanitizeEntity(entity, { model: strapi.models['interest-rate'] })
+  //   );
+  // },
+};
---
+++ api/spend-account/controllers/spend-account.js
@@ -0,0 +1,15 @@
+'use strict';
+// const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
+
+module.exports = {
+//   async create(ctx) {
+//     let entity;
+//     if (ctx.is("multipart")) {
+//       const { data, files } = parseMultipartData(ctx);
+//       entity = await strapi.services['spend-account'].create(data, { files });
+//     } else {
+//       entity = await strapi.services['spend-account'].create(ctx.request.body);
+//     }
+//     return sanitizeEntity(entity, { model: strapi.models['spend-account'] });
+//   },
+};
